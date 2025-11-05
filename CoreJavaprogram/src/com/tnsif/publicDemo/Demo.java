1655ece427217a44c11db3698e364d3ab93bd8cb
rangu <rangu@Sharadhamma>
2025-11-05 21:16:06 +0530

    first

---
+++ CoreJavaprogram/src/com/tnsif/publicDemo/Demo.java
@@ -0,0 +1,14 @@
+package com.tnsif.publicDemo;
+
+import com.tnsif.accessmodifierdemo.accessdemo;
+
+public class Demo {
+public static void main(String[] args) {
+		accessdemo obj=new accessdemo();
+		
+		System.out.println(obj.a);
+		System.out.println(obj.s);
+		
+		obj.display();
+	}
+	}
